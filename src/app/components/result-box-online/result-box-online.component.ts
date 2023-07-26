cc0d7865dcf4bcc03c935b8f84c308d9991ef81a
bachikhsoukaina <bachikhsoukaina@gmail.com>
2023-07-26 16:29:46 +0100

    creation of result page

---
+++ src/app/components/result-box-online/result-box-online.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-result-box-online',
+  templateUrl: './result-box-online.component.html',
+  styleUrls: ['./result-box-online.component.css']
+})
+export class ResultBoxOnlineComponent {
+
+}
