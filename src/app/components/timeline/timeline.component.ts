7a3827e10d7ccb13b93cdbe80833d42d9ea88dc9
ouahline.omaima <ouahline.omaima@ine.inpt.ma>
2023-07-27 12:38:48 +0100

    Adding the CIN input field

--- src/app/components/timeline/timeline.component.ts
+++ src/app/components/timeline/timeline.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-timeline',
   templateUrl: './timeline.component.html',
-  styleUrls: ['./timeline.component.css']
+  styleUrls: ['./timeline.component.css'],
 })
 export class TimelineComponent {
   @Input() classNames: Array<String> = [];
--- src/app/pages/choix-simulation/choix-simulation.component.ts
+++ src/app/pages/choix-simulation/choix-simulation.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-choix-simulation',
@@ -8,6 +9,7 @@ import { Router } from '@angular/router';
 })
 export class ChoixSimulationComponent {
   classNamesForTimeline: Array<string> = ['current-item', 'comming', 'comming'];
+  cin: string = '';
   constructor(private router: Router) {}
   goToAccueilPage() {
     this.router.navigate(['/accueil']);
