b9df0cdcaab6d8d5c8b8f5d9128c8fa0de4f2e55
ouahline.omaima <ouahline.omaima@ine.inpt.ma>
2023-07-29 15:08:23 +0100

    Adding the childs section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -58,7 +59,8 @@ const appRoutes : Routes = [
     MatInputModule,
     BrowserAnimationsModule,
     MatChipsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
