d0db69a9643190e6ca0305b3993f9ffa1df0ce9e
ouahline.omaima <ouahline.omaima@ine.inpt.ma>
2023-07-30 14:58:34 +0100

    Fixing the problem of date format

--- src/app/components/enfant/enfant.component.ts
+++ src/app/components/enfant/enfant.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { Child, SharedVariablesService } from 'src/app/services/shared-variables.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-enfant',
   templateUrl: './enfant.component.html',
-  styleUrls: ['./enfant.component.css']
+  styleUrls: ['./enfant.component.css'],
+  providers: [DatePipe]
 })
 export class EnfantComponent {
 
-  constructor(public sharedVariablesService: SharedVariablesService) { }
+  constructor(public sharedVariablesService: SharedVariablesService, private datePipe: DatePipe) { }
 
+  selectedDateOfBirthRaw: object | any = null;
   selectedDateOfBirth: Date | null = null;
   selectedIsCurrentlyStudying: boolean | null = null;
   selectedMarialStatus: string = '';
@@ -29,7 +32,11 @@ export class EnfantComponent {
   infirmities: Array<string> = ['Physique', 'Mentale']
   updateButtonState() {
     
-    
+  }
+
+  formatDate(){
+    this.selectedDateOfBirth = this.selectedDateOfBirthRaw?.toDate();
+
   }
 
   onSubmit(){
