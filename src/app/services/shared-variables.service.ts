ffba5d0d1dbe2cc67fb5d110fa3f9dffa25168e2
ouahline.omaima <ouahline.omaima@ine.inpt.ma>
2023-07-30 01:22:38 +0100

    Fixing the general layout of the child component

--- src/app/services/shared-variables.service.ts
+++ src/app/services/shared-variables.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
 import { SimulationType } from '../enums/SimulationType';
 
+
+export interface Child{
+  dateOfBirth: Date;
+  isCurrentlyStudying: boolean;
+  marialStatus: String;
+  isInfirm: boolean;
+  infirmityType: string;
+  
+}
+
 @Injectable({
   providedIn: 'root'
 })
+
+
+
 export class SharedVariablesService {
   cin: string = '';
   tel: string = '';
@@ -12,6 +25,9 @@ export class SharedVariablesService {
   isRetired: boolean | null = null;
   hasChildren: boolean | null = null;
   isValidMarriagePeriod: boolean | null = null;
+  children: Array<Child> = [];
+  childOrder: number = 1;
+  
 
   constructor() { }
 }
