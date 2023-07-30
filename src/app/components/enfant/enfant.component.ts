c13faa23407f0f857e25b2794b4813d86ec5329c
ouahline.omaima <ouahline.omaima@ine.inpt.ma>
2023-07-30 22:21:26 +0100

    Fixing the onSubmit function

--- src/app/components/enfant/enfant.component.ts
+++ src/app/components/enfant/enfant.component.ts
@@ -19,43 +19,48 @@ export class EnfantComponent {
   selectedIsInfirm: string | null = null;
   selectedInfirmityType: string | null = null;
   isButtonDisabled: boolean = true;
+  isDeclared: boolean = false;
 
-
-  child = {
-    dateOfBirth: this.selectedDateOfBirth,
-    isCurrentlyStudying: this.selectedIsCurrentlyStudying,
-    marialStatus: this.selectedMarialStatus,
-    isInfirm: this.selectedIsInfirm,
-    infirmityType: this.selectedInfirmityType
-  }
 
   infirmities: Array<string> = ['Physique', 'Mentale']
 
 
   updateButtonState() {
-    console.log('called');
-    const isDateOfBirthSelected = this.selectedDateOfBirthRaw !== null;
-    const isIsCurrentlyStudyingSelected = this.selectedIsCurrentlyStudying !== null;
-    const isMarialStatusSelected = this.selectedMarialStatus !== '';
-    const isIsInfirmSelected = this.selectedIsInfirm !== null;
+    if (!this.isDeclared) {
+      const isDateOfBirthSelected = this.selectedDateOfBirthRaw !== null;
+      const isIsCurrentlyStudyingSelected = this.selectedIsCurrentlyStudying !== null;
+      const isMarialStatusSelected = this.selectedMarialStatus !== '';
+      const isIsInfirmSelected = this.selectedIsInfirm !== null;
 
-    if (isIsInfirmSelected && this.selectedIsInfirm === 'true') {
-      this.isButtonDisabled =
-        !(isDateOfBirthSelected && isIsCurrentlyStudyingSelected && isMarialStatusSelected && this.selectedInfirmityType !== null);
-    } else if(isIsInfirmSelected && this.selectedIsInfirm === 'false') {
-      this.isButtonDisabled =
-        !(isDateOfBirthSelected && isIsCurrentlyStudyingSelected && isMarialStatusSelected);
+      if (isIsInfirmSelected && this.selectedIsInfirm === 'true') {
+        this.isButtonDisabled =
+          !(isDateOfBirthSelected && isIsCurrentlyStudyingSelected && isMarialStatusSelected && this.selectedInfirmityType !== null);
+      } else if (isIsInfirmSelected && this.selectedIsInfirm === 'false') {
+        this.isButtonDisabled =
+          !(isDateOfBirthSelected && isIsCurrentlyStudyingSelected && isMarialStatusSelected);
+      }
+
     }
-
 
 
   }
 
   formatDate() {
     this.selectedDateOfBirth = this.selectedDateOfBirthRaw?.toDate();
-
   }
 
   onSubmit() {
+    let child: Child = {
+      dateOfBirth: `${this.selectedDateOfBirth?.getDate().toString().padStart(2, '0')}/${(this.selectedDateOfBirth!.getMonth() + 1).toString().padStart(2, '0')}/${this.selectedDateOfBirth?.getFullYear()}`,
+      isCurrentlyStudying: this.selectedIsCurrentlyStudying === 'true',
+      marialStatus: this.selectedMarialStatus,
+      isInfirm: this.selectedIsInfirm === 'true',
+      infirmityType: this.selectedInfirmityType
+    }
+    this.sharedVariablesService.children.push(child)
+    this.sharedVariablesService.childOrder += 1;
+    console.log(this.sharedVariablesService.children, this.sharedVariablesService.childOrder);
+    this.isButtonDisabled = true;
+    this.isDeclared = true;
   }
 }
