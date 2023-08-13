4493d8ded06a8e77be9224e476ee98a6c7386a0d
ouahline.omaima <ouahline.omaima@ine.inpt.ma>
2023-08-13 22:22:45 +0100

    Built-in the result box logic

--- src/app/pages/resultat/resultat.component.ts
+++ src/app/pages/resultat/resultat.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SharedVariablesService } from 'src/app/services/shared-variables.service';
 
@@ -8,15 +8,70 @@ import { SharedVariablesService } from 'src/app/services/shared-variables.servic
   styleUrls: ['./resultat.component.css']
 })
 export class ResultatComponent {
+  childrenPourcentage: number = 0;
+  partnerPourcentage: number = 0;
+  childrenNames: string[] = [];
 
-  constructor(private router: Router, public sharedVariablesService: SharedVariablesService) {}
+  constructor(private router: Router, public sharedVariablesService: SharedVariablesService) { }
 
   classNamesForTimeline: Array<string> = ['done', 'done', 'current-item'];
 
+  ngOnInit(): void {
 
+    this.getStatus();
+  }
+
+  getStatus() {
+    if (this.sharedVariablesService.hasChildren != true) {
+      // cas de conjoint seul
+      if (this.sharedVariablesService.isValidMarriagePeriod === true) {
+        this.partnerPourcentage = 100;
+        return;
+      }
+      return;
+    }
+    else {
+      // cas des enfants
+      for (let child of this.sharedVariablesService.children) {
+        if( child.marialStatus === 'non marié'){
+          if (child.isInfirm === true) {
+            this.childrenNames.push(child.name);
+          }
+          else {
+            const age: number = this.calculateAge(child.dateOfBirth);
+            if (age < 16){
+              this.childrenNames.push(child.name);
+            }
+            else if (age < 21 && child.isCurrentlyStudying === true){
+              this.childrenNames.push(child.name);
+            }
+          }
+        }
+      }
+      
+
+    }
+
+  }
 
 
   
+  
+  calculateAge(birthdateString: string | null): number {
+    const currentDate = new Date();
+    const birthdateParts = birthdateString?.split('/');
+    const birthYear = birthdateParts ? parseInt(birthdateParts[2]) : 0;
+    const birthMonth = birthdateParts ? parseInt(birthdateParts[1]) - 1 : 0;
+    const birthDay = birthdateParts ? parseInt(birthdateParts[0]) : 0;
+  
+    const birthdate = new Date(birthYear, birthMonth, birthDay);
+    const ageMilliseconds = currentDate.getTime() - birthdate.getTime();
+    const ageDate = new Date(ageMilliseconds);
+    return Math.abs(ageDate.getUTCFullYear() - birthYear);
+  }
+
+
+
   goToAccueilPage() {
     this.router.navigate(['/accueil']);
   }
