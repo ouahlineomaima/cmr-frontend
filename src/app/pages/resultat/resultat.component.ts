8e9d77fd639dc609e7bd216e08ce58473919b53f
ouahline.omaima <ouahline.omaima@ine.inpt.ma>
2023-08-14 22:51:12 +0100

    Building the results logic

--- src/app/pages/resultat/resultat.component.ts
+++ src/app/pages/resultat/resultat.component.ts
@@ -31,39 +31,90 @@ export class ResultatComponent {
       return;
     }
     else {
+      let x: boolean = false; // to know wheter at least one of the children is elligible or not
       // cas des enfants
       for (let child of this.sharedVariablesService.children) {
-        if( child.marialStatus === 'non marié'){
+        if (child.marialStatus === 'non marié') {
           if (child.isInfirm === true) {
             this.childrenNames.push(child.name);
+            x = true
           }
           else {
             const age: number = this.calculateAge(child.dateOfBirth);
-            if (age < 16){
+            if (age < 16) {
               this.childrenNames.push(child.name);
+              x = true
             }
-            else if (age < 21 && child.isCurrentlyStudying === true){
+            else if (age < 21 && child.isCurrentlyStudying === true) {
               this.childrenNames.push(child.name);
+              x = true
             }
           }
         }
       }
-      
 
+      // traitement de conjoint
+      if (this.sharedVariablesService.isStillPartner === true) {
+        switch (this.sharedVariablesService.partnerMarialStatus) {
+          case 'veuve':
+            if (x == true) {
+              this.partnerPourcentage = 50
+            }
+            else {
+              this.partnerPourcentage = 10
+            }
+            break;
+          case 'Divorcé':
+          case 'Divorcée':
+          case 'Remariée':
+          case 'Remarié':
+          case 'Répudiée':
+            this.partnerPourcentage = 0;
+            break;
+
+          case 'veuf':
+            if (this.sharedVariablesService.isPartnerInfirm === true) {
+              if (x == true) {
+                this.partnerPourcentage = 50
+                break;
+              }
+              else {
+                this.partnerPourcentage = 100
+                break;
+              }
+            }
+            if (this.sharedVariablesService.isPartnerInfirm === false) {
+              if (this.sharedVariablesService.isPartnerRetired === true) {
+                if (x == true) {
+                  this.partnerPourcentage = 50
+                  break;
+                }
+                else {
+                  this.partnerPourcentage = 100
+                  break;
+                }
+              }
+              else {
+                this.partnerPourcentage = -1
+                break;
+              }
+              break;
+            }
+        }
+      }
     }
-
   }
 
 
-  
-  
+
+
   calculateAge(birthdateString: string | null): number {
     const currentDate = new Date();
     const birthdateParts = birthdateString?.split('/');
     const birthYear = birthdateParts ? parseInt(birthdateParts[2]) : 0;
     const birthMonth = birthdateParts ? parseInt(birthdateParts[1]) - 1 : 0;
     const birthDay = birthdateParts ? parseInt(birthdateParts[0]) : 0;
-  
+
     const birthdate = new Date(birthYear, birthMonth, birthDay);
     const ageMilliseconds = currentDate.getTime() - birthdate.getTime();
     const ageDate = new Date(ageMilliseconds);
