ffc498ba1a05b5f322acf23909f23521d97ef6b5
ouahline.omaima <ouahline.omaima@ine.inpt.ma>
2023-08-15 23:28:35 +0100

    Starting building the journey logic

--- src/app/pages/parcours/parcours.component.ts
+++ src/app/pages/parcours/parcours.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { Event } from 'src/app/interfaces/event';
 import { Layer } from 'src/app/interfaces/layer';
 
-
 @Component({
   selector: 'app-parcours',
   templateUrl: './parcours.component.html',
@@ -10,10 +9,44 @@ import { Layer } from 'src/app/interfaces/layer';
 })
 export class ParcoursComponent {
 
-  titles: Array<string> = ['chez-soi', 'District'];
-  titlesBgColors: Array<string> = ['#FFF28C', '#6DE7B6'];
-  titlesColors: Array<string> = ['#80690B', '#0F7950'];
-  containerBgColors: Array<string> = ['#FFFAC2', '#D0FAE4'];
+
+
+  titles: Array<string> = ['chez-soi', "District: Bureau d'état civil", "Service notarial", "Banque", "Hôpital", "Tribunal", "CMR: site web", "CMR: Délégation régionale", "Ecole"];
+  titlesBgColors: Array<string> = ['#FFF28C', '#6DE7B6', '#7ED3FC', '#C5B5FC', '#FEB974', '#904C77', "#0B7189", '#03045E', '#CEB5A7'];
+  titlesColors: Array<string> = ['#80690B', '#0F7950', '#045884', '#4D2892', '#91390B', '#0B7189', '#063F4B', '#010114', '#B6917C'];
+  containerBgColors: Array<string> = ['#FFFAC2', '#D0FAE4', '#E1F3FF', '#EEEBFF', '#FFEDD5', '#B979A1', '#12BCE2', '#0608B2', '#F0E9E5'];
+  CNIEArray: Array<string> = [];
+
+demandeDeReversion: Event = {
+  title: 'Demande de réversion datée et signée',
+  iconColor: this.titlesBgColors[6],
+  eventColor: this.titlesColors[6],
+  details: ''
+}
+
+acteDeDeces: Event = {
+  title: "Extrait de l'acte du décès du défunt (e)",
+  iconColor: this.titlesBgColors[1],
+  eventColor: this.titlesColors[1],
+  details: `Livret de famille ou livret d'identification d'état civil.
+  Examen du certifical médicale de décès.`
+}
+
+certifDeDeces: Event = {
+  title: "Examen du certifical médicale de décès",
+  iconColor: this.titlesBgColors[4],
+  eventColor: this.titlesColors[4],
+  details: ""
+}
+
+CNIE: Event = {
+  title: "Photocopie de la CNIE",
+  iconColor: this.titlesBgColors[0],
+  eventColor: this.titlesColors[0],
+  details: `Photocopie de la CNIE de ${this.CNIEArray.join(', ')}`
+}
+
+
   event1: Event = {
     title: 'event1',
     iconColor: this.titlesBgColors[0],
